f37bb82c9219bf762149ada5047a6af81906858f
Selin Tanla Rabia Mercan <73990724+SelinTanlaRabiaMercan@users.noreply.github.com>
2022-11-28 15:51:10 +0300

    Delete JAVA102 directory

--- JAVA102/kitapSiralayici/src/sayfasayisi.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class sayfasayisi<S> implements Comparator<Book> {
-    @Override
-    public int compare(Book o1, Book o2) {
-        return o1.getNumberOfPages()-o2.getNumberOfPages();
-    }
-}
