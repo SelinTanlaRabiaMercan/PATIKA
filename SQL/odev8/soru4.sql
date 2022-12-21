aa5ef580218754943307bfbc3f1f26d92342a5cc
Selin Tanla Rabia Mercan <73990724+SelinTanlaRabiaMercan@users.noreply.github.com>
2022-12-21 15:06:06 +0300

    Create soru4.sql

---
+++ SQL/odev8/soru4.sql
@@ -0,0 +1,16 @@
+Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+
+DELETE FROM employee
+WHERE id=15;
+
+DELETE FROM employee
+WHERE id=45;
+
+DELETE FROM employee
+WHERE id=32;
+
+DELETE FROM employee
+WHERE name='Mommy';
+
+DELETE FROM employee
+WHERE id=6;
